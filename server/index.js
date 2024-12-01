ea3a4efaf4b8be3ce8176fe24fff3809ae53b0ec
Akash Sen <asen05897@gmail.com>
2024-12-01 19:58:51 +0530

    use express

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");// video 8 url handaling
 
+const express = require("express");
+
 // video 7 http server basics
+/*
 const myServer = http.createServer((req,res) =>{
     //console.log("New request rec.");
     //console.log(req.headers); 
-    /*Server Started output of req.headers from client site
+    Server Started output of req.headers from client site
 {
   host: 'localhost:8000',
   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0',    
@@ -85,7 +88,7 @@ const log = `${Date.now()} ${req.url} the new user Received\n`; //log file it co
     })
     //res.end("hello from the server");
 })
-*/
+
 
 //video 9 http methods
 //get , post , punch , put , delete (all you u can say those all are the CRUD operation)
@@ -127,5 +130,59 @@ const log = `${Date.now()} ${req.method} ${req.url} the new user Received\n`; //
     })
     //res.end("hello from the server");
 })
+
+})
+*/
+
+//video 10 work with express
+
+const app = express();
+
+app.get("/", (req,res) =>{
+    return res.end("Hello from the Home page");
+})
+
+app.get("/about", (req,res) =>{
+    return res.end(`Hii ${req.query.name}`); //http://localhost:8000/about?name=Akash
+})
+
+function myHandler(req,res){
+    const log = `${Date.now()} ${req.method} ${req.url} the new user Received\n`; //log file it contais all info 
+    if(req.url === "/favicon.ico") return res.end();
+    const myUrl = url.parse(req.url,true);
+    //console.log(myUrl);
+    fs.appendFile("./log.txt", log, (error, data) =>{
+        //res.end("Hello from the server");
+        switch(myUrl.pathname){
+            case "/":
+                res.end("Home Page");
+                break;
+            case "/about": 
+                res.end("hii my name is Akash");
+                break;
+            case "/signup":
+                if(req.method === "GET") return res.end("this is the signup page");
+                else if(req.method === "POST"){
+                    //DB Query
+                    res.end("Success");
+                }
+                break;
+            case "/search":
+                const search = myUrl.query.search_query; // this is search query
+                res.end(`Here is your recommended search ` + search); 
+                
+                break;
+            default:
+                res.end("Error 404!!");
+                break;
+
+        }
+    })
+    //res.end("hello from the server");
+}
+
+const myServer = http.createServer(app);
+
+
 
 myServer.listen(8000, () => console.log("Server Started"));
